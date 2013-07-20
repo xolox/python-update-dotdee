954709fc0e43d412d8ff3a9d5a50f52bf134fb3b
Peter Odding <peter@peterodding.com>
2013-07-21 00:52:34 +0200

    Pin requirements

--- setup.py
+++ setup.py
@@ -3,10 +3,16 @@
 from os.path import abspath, dirname, join
 from setuptools import setup
 
+# Find the directory where the source distribution was unpacked.
+source_directory = dirname(abspath(__file__))
+
 # Fill in the long description (for the benefit of PyPi)
 # with the contents of README.rst (rendered by GitHub).
-readme_file = join(dirname(abspath(__file__)), 'README.rst')
+readme_file = join(source_directory, 'README.rst')
 readme_text = open(readme_file, 'r').read()
+
+# Fill in the "install_requires" field based on requirements.txt.
+requirements = [l.strip() for l in open(join(source_directory, 'requirements.txt'), 'r')]
 
 setup(name='update-dotdee',
       version='1.0.5',
@@ -17,4 +23,4 @@ setup(name='update-dotdee',
       author_email='peter@peterodding.com',
       py_modules=['update_dotdee'],
       entry_points={'console_scripts': ['update-dotdee = update_dotdee:main']},
-      install_requires=['coloredlogs', 'humanfriendly', 'naturalsort'])
+      install_requires=requirements)
