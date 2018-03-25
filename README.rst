566c4c92fdd33734c521f9d176b91ff6ba1903ee
Peter Odding <peter@peterodding.com>
2018-03-25 15:53:52 +0200

    Bump copyright

--- README.rst
+++ README.rst
@@ -147,7 +147,7 @@ License
 
 This software is licensed under the `MIT license`_.
 
-© 2017 Peter Odding.
+© 2018 Peter Odding.
 
 .. External references:
 .. _/etc/crypttab: https://manpages.debian.org/crypttab
--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 # Sphinx documentation configuration for the `update-dotdee' package.
 #
 # Author: Peter Odding <peter@peterodding.com>
-# Last Change: July 12, 2017
+# Last Change: March 25, 2018
 # URL: https://github.com/xolox/python-update-dotdee
 
 """Sphinx documentation configuration for the `update-dotdee` package."""
@@ -36,7 +36,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'update-dotdee'
-copyright = '2017, Peter Odding'
+copyright = '2018, Peter Odding'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
