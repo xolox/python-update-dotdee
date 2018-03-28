04d5836f0d217e32778745b533beeb8159d80c32
Peter Odding <peter@peterodding.com>
2018-03-29 01:21:09 +0200

    Release 5.0: Support *.ini configuration file loading

--- update_dotdee/__init__.py
+++ update_dotdee/__init__.py
@@ -41,7 +41,7 @@ from property_manager import (
 from six.moves import configparser
 
 # Semi-standard module versioning.
-__version__ = '4.0'
+__version__ = '5.0'
 
 DOCUMENTATION_TEMPLATE = """
 Configuration files are text files in the subset of `ini syntax`_ supported by
