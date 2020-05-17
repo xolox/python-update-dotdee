96274d5877fd5b64031c253f78936eed589d459f
Peter Odding <peter@peterodding.com>
2020-05-17 02:14:51 +0200

    Add license=MIT and 'python_requires' to setup script (and apply Black)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # Setup script for the `update-dotdee' package.
 #
 # Author: Peter Odding <peter@peterodding.com>
-# Last Change: July 12, 2017
+# Last Change: May 17, 2020
 # URL: https://github.com/xolox/python-update-dotdee
 
 """
@@ -30,15 +30,15 @@ from setuptools import find_packages, setup
 
 def get_contents(*args):
     """Get the contents of a file relative to the source distribution directory."""
-    with codecs.open(get_absolute_path(*args), 'r', 'UTF-8') as handle:
+    with codecs.open(get_absolute_path(*args), "r", "UTF-8") as handle:
         return handle.read()
 
 
 def get_version(*args):
     """Extract the version number from a Python module."""
     contents = get_contents(*args)
-    metadata = dict(re.findall('__([a-z]+)__ = [\'"]([^\'"]+)', contents))
-    return metadata['version']
+    metadata = dict(re.findall("__([a-z]+)__ = ['\"]([^'\"]+)", contents))
+    return metadata["version"]
 
 
 def get_requirements(*args):
@@ -47,10 +47,10 @@ def get_requirements(*args):
     contents = get_contents(*args)
     for line in contents.splitlines():
         # Strip comments.
-        line = re.sub(r'^#.*|\s#.*', '', line)
+        line = re.sub(r"^#.*|\s#.*", "", line)
         # Ignore empty lines
         if line and not line.isspace():
-            requirements.add(re.sub(r'\s+', '', line))
+            requirements.add(re.sub(r"\s+", "", line))
     return sorted(requirements)
 
 
@@ -59,42 +59,44 @@ def get_absolute_path(*args):
     return os.path.join(os.path.dirname(os.path.abspath(__file__)), *args)
 
 
-setup(name='update-dotdee',
-      version=get_version('update_dotdee', '__init__.py'),
-      description="Generic modular configuration file manager",
-      long_description=get_contents('README.rst'),
-      url='https://github.com/xolox/python-update-dotdee',
-      author="Peter Odding",
-      author_email='peter@peterodding.com',
-      packages=find_packages(),
-      entry_points=dict(console_scripts=[
-          'update-dotdee = update_dotdee.cli:main',
-      ]),
-      install_requires=get_requirements('requirements.txt'),
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Environment :: Console',
-          'Intended Audience :: Developers',
-          'Intended Audience :: Information Technology',
-          'Intended Audience :: System Administrators',
-          'License :: OSI Approved :: MIT License',
-          'Natural Language :: English',
-          'Operating System :: POSIX',
-          'Operating System :: POSIX :: Linux',
-          'Operating System :: Unix',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: Implementation :: CPython',
-          'Programming Language :: Python :: Implementation :: PyPy',
-          'Topic :: Software Development',
-          'Topic :: Software Development :: Libraries',
-          'Topic :: Software Development :: Libraries :: Python Modules',
-          'Topic :: System :: Systems Administration',
-          'Topic :: Utilities',
-      ])
+setup(
+    name="update-dotdee",
+    version=get_version("update_dotdee", "__init__.py"),
+    description="Generic modular configuration file manager",
+    long_description=get_contents("README.rst"),
+    url="https://update-dotdee.readthedocs.io",
+    author="Peter Odding",
+    author_email="peter@peterodding.com",
+    license="MIT",
+    packages=find_packages(),
+    entry_points=dict(console_scripts=["update-dotdee = update_dotdee.cli:main"]),
+    install_requires=get_requirements("requirements.txt"),
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Utilities",
+    ],
+)
